dff963addfad82d9a8efb06add70751b3715a787
Jconrega <xema11.1.96@gmail.com>
2015-03-17 13:48:25 +0100

    operator

--- Project1/Project1/ArrayDynamic.h
+++ Project1/Project1/ArrayDynamic.h
@@ -1,6 +1,10 @@
 #ifdef _ARRAYDYNAMIC_H_
 #define _ARRAYDYNAMIC_H_
 
+#include <assert.h>
+#include <stdio.h>
+
+
 class ArrayDynamic
 {
 private:
@@ -26,17 +30,42 @@ public:
 			delete[]data;
 	}
 	//Realocatar memoria
-	void ModifyMemory(unsigned int new_memory_size);
+	void ModifyMemory(unsigned int new_memory_size)
+	{
+		if (new_memory_size !=NULL){
+			if (new_memory_size>allocated_memory || allocated_memory ==NULL){
+				allocated_memory = new_memory_size;
+			}
+		}
+	}
 	//Afegir Memoria
-	void PushBack(int value);
+	void PushBack(int value){
+		if(value != NULL){
+			
+			if (num_elements + 1>allocated_memory){
+				
+				ModyfyMemory(num_elements+1);
+			}
+			data[num_elements]=value;
+			num_elements++;
+			allocated_memory++;
+		}
+	
+	}
 	//ExtreuMemoria
 	int Pop();
 	//Canvia la posicio que te per un valor
 	void Insert(int value, unsigned int position);
 	//Torna referencia no constant
-	int& operator[](unsigned int index);
+	int& operator[](unsigned int index){
+		assert(index < num_elements);
+		return data[index];
+	}
 	//sobre carga de operador. Torna referencia constant
-	const int& operator [](unsigned int index)const;
+	const int& operator [](unsigned int index)const{
+		assert(index < num_elements);
+		return data[index];
+	}
 };
 
 #endif // _ARRAYDYNAMIC_H_
